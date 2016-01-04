a10d0112ea81fe791578e31bdb57e5f88fb55969
Johnny Hall <johnny@recipher.co.uk>
2016-01-04 15:06:11 +0000

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,86 @@
+var should = require('chai').should()
+  , utility = require('../lib');
+
+describe('utility', function() {
+  
+  describe('random', function() {
+    
+    before(function() {
+    });
+    
+    it('should ', function() {
+    });
+
+  });
+  
+  describe('encrypt', function() {
+
+    before(function() {
+    });
+    
+    it('should ', function() {
+    });
+
+  });
+  
+  describe('decrypt', function() {
+
+    before(function() {
+    });
+    
+    it('should ', function() {
+    });
+
+  });
+  
+  describe('token', function() {
+
+    before(function() {
+    });
+    
+    it('should ', function() {
+    });
+
+  });
+  
+  describe('slugify', function() {
+
+    before(function() {
+    });
+    
+    it('should ', function() {
+    });
+
+  });
+  
+  describe('percentage', function() {
+
+    before(function() {
+    });
+    
+    it('should ', function() {
+    });
+
+  });
+  
+  describe('getApp', function() {
+
+    before(function() {
+    });
+    
+    it('should ', function() {
+    });
+
+  });
+  
+  describe('folders', function() {
+
+    before(function() {
+    });
+    
+    it('should ', function() {
+    });
+
+  });
+  
+});
