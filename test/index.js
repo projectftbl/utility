b7dc0e5197219d9e988ad39f28d6eb9b81108f82
Johnny Hall <johnny@recipher.co.uk>
2016-05-03 15:38:46 +0100

    Remove constructUsers

--- test/index.js
+++ test/index.js
@@ -86,47 +86,4 @@ describe('utility', function() {
 
   });  
 
-  describe('constructUsers', function() {
-
-    var users = [
-      'system/password/system'
-    , 'guest/secret/guest' 
-    ];
-
-    var roles = [
-      { name: 'Guest', handle: 'guest', claims: [ 
-          { entity: 'member', 'right': 1 } 
-        , { entity: 'user', 'right': 1 } 
-        ] 
-      }
-    , { name: 'System', handle: 'system', claims: [ 
-          { entity: 'member', right: 15 } 
-        , { entity: 'user', right: 15 } 
-        , { entity: 'system', right: 15 } 
-        ]
-      }
-    ];
-
-    before(function() {
-      suite.users = utility.constructUsers(users, roles);
-    });
-    
-    it('should generate 2 users', function() {
-      suite.users.length.should.equal(2);
-    });
-    
-    it('should generate guest user', function() {
-      _(suite.users).find({ name: 'guest' }).should.exist;
-    });
-    
-    it('should generate system user', function() {
-      _(suite.users).find({ name: 'system' }).should.exist;
-    });
-    
-    it('should not generate support user', function() {
-      should.not.exist(_(suite.users).find({ name: 'support' }));
-    });
-
-  });
-  
 });
